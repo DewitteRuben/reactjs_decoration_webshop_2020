ae3b1b69afc3145361c3995f214e7944fdc13cb7
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-23 22:05:19 +0100

    refactor: whitespace

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -6,7 +6,7 @@ const StyledContainer = styled.div`
   margin: 0 auto;
   display: grid;
   grid-template-columns: 290px 1fr;
-  grid-template-areas: 
+  grid-template-areas:
     "breadcrumbs breadcrumbs"
     "aside list"
     "aside list";
