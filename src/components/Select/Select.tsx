1c643c5ea1e55b3024725460262fcfc13f02522e
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-24 10:56:43 +0100

    bugfix: select now appears above ItemCards

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -19,6 +19,7 @@ const SelectContainer = styled.div`
 const ItemContainer = styled.div`
   position: absolute;
   border: 1px solid #ddd;
+  z-index: 10;
   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
   margin-top: 8px;
   background: #fff;
