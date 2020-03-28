9fe0a1d9f2cb923705f8810c806ec405f1454509
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-28 17:39:25 +0100

    feat: created generic button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { rem, lighten } from "polished";
+
+const Button = styled.button`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${props => props.theme.black};
+  background-color: initial;
+  color: ${props => props.theme.black};
+  font-weight: 700;
+  font-size: ${rem(18)};
+  padding: ${rem(10)} ${rem(20)};
+  margin: ${rem(10)} 0;
+  position: relative;
+  box-shadow: 0 2px ${props => lighten(0.2)(props.theme.black)};
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 1px ${props => lighten(0.2)(props.theme.black)};
+    top: 1px;
+  }
+
+  &:active {
+    box-shadow: 0 0px ${props => lighten(0.2)(props.theme.black)};
+
+    top: 3px;
+  }
+`;
+
+export default Button;
