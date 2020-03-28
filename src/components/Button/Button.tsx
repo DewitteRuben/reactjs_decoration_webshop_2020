3d33d974e1be684135ada080e5d9cfed0d7a0e09
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-28 22:21:33 +0100

    ui: changed button border color

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid ${props => props.theme.black};
+  border: 1px solid ${props => props.theme.darkGray};
   background-color: initial;
   color: ${props => props.theme.black};
   font-weight: 700;
@@ -14,16 +14,16 @@ const Button = styled.button`
   padding: ${rem(10)} ${rem(20)};
   margin: ${rem(10)} 0;
   position: relative;
-  box-shadow: 0 2px ${props => lighten(0.2)(props.theme.black)};
+  box-shadow: 0 2px ${props => lighten(0.2)(props.theme.darkGray)};
 
   &:hover {
     cursor: pointer;
-    box-shadow: 0 1px ${props => lighten(0.2)(props.theme.black)};
+    box-shadow: 0 1px ${props => lighten(0.2)(props.theme.darkGray)};
     top: 1px;
   }
 
   &:active {
-    box-shadow: 0 0px ${props => lighten(0.2)(props.theme.black)};
+    box-shadow: 0 0px ${props => lighten(0.2)(props.theme.darkGray)};
 
     top: 3px;
   }
