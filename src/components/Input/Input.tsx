07ad8fec9286c730c8ecfc6e89e73ff0246a94c7
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-31 16:41:07 +0200

    feat: added error message system to text inputs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import Icon, { IconTypes } from "../Icon/Icon";
 
-const Item = styled.input`
+const Input = styled.input`
   background: none;
   border: none;
   padding: 0 ${rem(5)} 0 ${rem(20)};
@@ -12,14 +12,21 @@ const Item = styled.input`
   font-size: ${rem(16)};
 `;
 
-const Label = styled.label`
+interface ILabelProps {
+  error?: boolean;
+}
+
+const Label = styled.label<ILabelProps>`
   position: absolute;
   left: ${rem(13)};
   font-size: ${rem(16)};
   background-color: ${props => props.theme.white};
+  color: ${props => (props.error ? props.theme.error : props.theme.black)};
   transform-origin: top left;
   padding 0 ${rem(5)};
   pointer-events: none;
+
+  transition: color 150ms ease-in;
 `;
 
 const scaleAndTranslate = keyframes`
@@ -52,48 +59,89 @@ const InputContainer = styled.div<IInputContainerProps>`
   background-color: ${props => props.theme.white};
   padding: ${rem(4)} ${rem(2)};
   border-radius: ${rem(25)};
-  border: 1px solid ${props => props.theme.darkGray};
+  border: 1px solid ${props => (props.error ? props.theme.error : props.theme.darkGray)};
+  transition: border 150ms ease-in;
 
-  ${Item}:focus + label {
+  ${Input}:focus + label {
     animation: ${scaleAndTranslate} 150ms cubic-bezier(0.4, 0, 0.2, 1) forwards;
   }
 
-  ${Item} + label {
+  ${Input} + label {
     animation: ${props => (props.animateLabel ? scaleAndTranslate : defaultLabelPosition)} 150ms cubic-bezier(0.4, 0, 0.2, 1)
       forwards;
   }
 `;
 
+interface IErrorMessageProps {
+  error?: boolean;
+}
+
+const ErrorMessage = styled.span<IErrorMessageProps>`
+  position: absolute;
+  bottom: -18px;
+  left: 15px;
+  font-size: 12px;
+  color: ${props => props.theme.error};
+`;
+
 interface IInputContainerProps {
   animateLabel?: boolean;
+  error?: boolean;
 }
 
 interface IProps extends IInputContainerProps {
   icon?: IconTypes;
   iconSize?: number;
   label?: string;
+  value?: string;
+  validate?: (value: string) => boolean;
+  errorMessage?: string;
+  onChangeText?: (value: string) => void;
 }
 
-const Input: React.FC<IProps & React.InputHTMLAttributes<HTMLInputElement>> = ({
+type CombinedProps = IProps & React.InputHTMLAttributes<HTMLInputElement>;
+
+const TextInput: React.FC<Omit<CombinedProps, "onChange">> = ({
   icon,
   iconSize = 28,
   label,
   className,
+  validate,
+  errorMessage,
+  onChangeText,
+  value,
   ...props
 }) => {
-  const [value, setValue] = React.useState("");
+  const [currentVal, setCurrentVal] = React.useState("");
 
-  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
+  const validationCallback = React.useCallback(() => {
+    if (currentVal.length && validate && errorMessage?.length) {
+      return validate(currentVal);
+    }
+    return false;
+  }, [currentVal, errorMessage, validate]);
+
+  const isInvalid = React.useMemo(() => validationCallback(), [validationCallback]);
+
+  const onChangeHandler = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = event.target.value;
+      setCurrentVal(inputValue);
+      if (onChangeText) {
+        onChangeText(inputValue);
+      }
+    },
+    [onChangeText]
+  );
 
   return (
-    <InputContainer className={className} animateLabel={Boolean(value.length)}>
-      <Item type="text" onChange={onChangeHandler} value={value} {...props} />
-      {label && <Label>{label}</Label>}
+    <InputContainer className={className} error={isInvalid} animateLabel={Boolean(currentVal.length)}>
+      <Input onChange={onChangeHandler} value={value || currentVal} {...props} />
+      {label && <Label error={isInvalid}>{label}</Label>}
       {icon && iconSize && <InputIcon name={icon} size={iconSize} />}
+      {isInvalid && <ErrorMessage>{errorMessage}</ErrorMessage>}
     </InputContainer>
   );
 };
 
-export default Input;
+export default TextInput;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,8 @@ const themes = {
     darkGray: "#949494",
     border: "#e6e6e7",
     opacityGray: "rgba(148, 148, 148, 0.1)",
-    black: "#171717"
+    black: "#171717",
+    error: "#B00020"
   }
 };
 
