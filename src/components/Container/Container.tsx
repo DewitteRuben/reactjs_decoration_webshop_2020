ec019292cceb5bafd4a3c332ada5f28abc5953ab
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-03-31 13:18:27 +0200

    refactor: made container component more generic

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,14 +4,8 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   max-width: 1440px;
   margin: 0 auto;
-  display: grid;
-  grid-template-columns: 290px 1fr;
-  grid-template-areas:
-    "breadcrumbs breadcrumbs"
-    "aside list"
-    "aside list";
 `;
 
-const Container: React.FC = ({ children }) => <StyledContainer>{children}</StyledContainer>;
+const Container: React.FC = ({ children, ...other }) => <StyledContainer {...other}>{children}</StyledContainer>;
 
 export default Container;
---
+++ src/components/GridContainer/GridContainer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import Container from "../Container/Container";
+
+const StyledContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 290px 1fr;
+  grid-template-areas:
+    "breadcrumbs breadcrumbs"
+    "aside list"
+    "aside list";
+`;
+
+const GridContainer: React.FC = ({ children, ...other }) => <StyledContainer {...other}>{children}</StyledContainer>;
+
+export default GridContainer;
