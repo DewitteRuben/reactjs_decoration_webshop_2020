d156c1191b0e98384aa5e03258d9bbf9643e2118
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-04-02 18:45:41 +0200

    feat: made cartstore dynamic

--- src/store/CartStore.ts
+++ src/store/CartStore.ts
@@ -1,57 +1,35 @@
-import { observable, action } from "mobx";
+import { observable, action, computed } from "mobx";
 import _ from "lodash";
 import { IShopItem } from "../io-ts-types";
 
 export class CartStore {
   @observable
-  items: IShopItem[] = [
-    {
-      id: "3KDSKQSL",
-      _id: "0",
-      description: "Beautiful plant",
-      images: {
-        larqe: "",
-        medium: "",
-        thumb: "",
-        small:
-          "https://images.pexels.com/photos/3603817/pexels-photo-3603817.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=44"
-      },
-      name: "Vase 420",
-      price: 22.5,
-      stateOfProduct: "good",
-      category: "decoration",
-      subCategory: "vases-and-flowers",
-      itemCategory: "vases",
-      specificCategory: "keramic-vase"
-    },
-    {
-      id: "KDJS@32DSK",
-      _id: "1",
-      description: "Beautiful plant",
-      images: {
-        larqe: "",
-        medium: "",
-        thumb: "",
-        small:
-          "https://images.pexels.com/photos/3603817/pexels-photo-3603817.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=44"
-      },
-      name: "Vase 420",
-      price: 22.5,
-      stateOfProduct: "good",
-      category: "decoration",
-      subCategory: "vases-and-flowers",
-      itemCategory: "vases",
-      specificCategory: "keramic-vase"
-    }
-  ];
+  items: IShopItem[] = [];
 
   @action
   addItem = (item: IShopItem) => {
-    this.items.push(item);
+    const hasItem = this.items.findIndex(itemInList => itemInList.id === item.id);
+    if (hasItem < 0) {
+      this.items.push(item);
+    }
   };
 
   @action
   removeItem = (id: string) => {
     _.remove(this.items, item => item._id === id);
   };
+
+  @computed
+  get totalPrice() {
+    if (!this.items.length) return "0.00";
+    return this.items
+      .map(item => item.price)
+      .reduce((prev, cur) => prev + cur)
+      .toFixed(2);
+  }
+
+  @computed
+  get hasItem() {
+    return this.items.length > 0;
+  }
 }
