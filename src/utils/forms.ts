e51479a576f2175ee9ebd432f27194a407081fcf
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-04-07 11:38:47 +0200

    feat: serializeFormData now correctly parses number, checkbox and file inputs

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,12 +1,59 @@
-const serializeFormData = <T extends {}>(formData: FormData): Partial<T> => {
+import _ from "lodash";
+
+interface IFormEntry {
+  [x: string]: FormDataEntryValue | boolean | number | File[];
+  [x: number]: FormDataEntryValue;
+}
+
+const filterFileEntries = (input: HTMLInputElement) => {
+  if (!input.files) return [];
+
+  const files = Array.from(input.files);
+  const included = input.dataset.included?.split(",");
+  if (included) {
+    return files.filter(file => included.includes(file.name));
+  }
+  return files;
+};
+
+const serializeFormData = <T extends {}>(form: HTMLFormElement): Partial<T> => {
+  const formData = new FormData(form);
   const entries = Array.from(formData.entries());
-  entries
-    .filter(arr => arr.indexOf("on") > -1)
-    .forEach(arr => {
-      const filteredArr = arr as [string, string | boolean];
-      filteredArr[1] = true;
+  const formElements = Array.from(form.elements) as HTMLInputElement[];
+
+  const fileInput = _.find(formElements, ["type", "file"]);
+  const numberInputs = _.filter(formElements, ["type", "number"]);
+  const checkboxInputs = _.filter(formElements, ["type", "checkbox"]);
+
+  let seralizedData = Object.fromEntries(entries) as IFormEntry;
+
+  // parse file input
+  if (fileInput && fileInput.name && fileInput.files) {
+    seralizedData = _.omit(seralizedData, fileInput.name);
+    seralizedData[fileInput.name] = filterFileEntries(fileInput);
+  }
+
+  // parse number inputs
+  if (numberInputs) {
+    numberInputs.forEach(input => {
+      const name = input.name;
+      if (name?.length) {
+        seralizedData[name] = parseFloat(seralizedData[name] as string) || 0;
+      }
     });
-  return (Object.fromEntries(entries) as unknown) as Partial<T>;
+  }
+
+  // parse checkbox inputs
+  if (checkboxInputs) {
+    checkboxInputs.forEach(input => {
+      const name = input.name;
+      if (name?.length) {
+        seralizedData[name] = input.checked;
+      }
+    });
+  }
+
+  return (seralizedData as unknown) as Partial<T>;
 };
 
 export { serializeFormData };
