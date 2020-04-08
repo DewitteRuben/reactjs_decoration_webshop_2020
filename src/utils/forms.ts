e789b482fe8abfd3ffde1051d83d765ec8148401
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-04-09 00:04:06 +0200

    feat: added date input support to form serialize + deep compare object method

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,7 +1,7 @@
 import _ from "lodash";
 
 interface IFormEntry {
-  [x: string]: FormDataEntryValue | boolean | number | File[];
+  [x: string]: FormDataEntryValue | boolean | number | File[] | Date;
   [x: number]: FormDataEntryValue;
 }
 
@@ -24,6 +24,7 @@ const serializeFormData = <T extends {}>(form: HTMLFormElement): Partial<T> => {
   const fileInput = _.find(formElements, ["type", "file"]);
   const numberInputs = _.filter(formElements, ["type", "number"]);
   const checkboxInputs = _.filter(formElements, ["type", "checkbox"]);
+  const dateInputs = _.filter(formElements, ["type", "date"]);
 
   let seralizedData = Object.fromEntries(entries) as IFormEntry;
 
@@ -53,7 +54,45 @@ const serializeFormData = <T extends {}>(form: HTMLFormElement): Partial<T> => {
     });
   }
 
+  // parse date inputs
+  if (dateInputs) {
+    dateInputs.forEach(input => {
+      const name = input.name;
+      if (name?.length && input.valueAsDate) {
+        seralizedData[name] = input.valueAsDate;
+      }
+    });
+  }
+
   return (seralizedData as unknown) as Partial<T>;
 };
 
-export { serializeFormData };
+/**
+ * Deep diff between two objects - i.e. an object with the new value of new & changed fields.
+ * Removed fields will be set as undefined on the result.
+ * Only plain objects will be deeply compared (@see _.isPlainObject)
+ *
+ * Inspired by: https://gist.github.com/Yimiprod/7ee176597fef230d1451#gistcomment-2565071
+ * This fork: https://gist.github.com/TeNNoX/5125ab5770ba287012316dd62231b764/
+ *
+ * @param  {Object} base   Object to compare with (if falsy we return object)
+ * @param  {Object} object Object compared
+ * @return {Object}        Return a new object who represent the changed & new values
+ */
+const deepDiffObj = (base: any, object: any) => {
+  if (!object) throw new Error(`The object compared should be an object: ${object}`);
+  if (!base) return object;
+  const result = _.transform(object, (result: any, value: any, key: any) => {
+    if (!_.has(base, key)) result[key] = value; // fix edge case: not defined to explicitly defined as undefined
+    if (!_.isEqual(value, base[key])) {
+      result[key] = _.isPlainObject(value) && _.isPlainObject(base[key]) ? deepDiffObj(base[key], value) : value;
+    }
+  });
+  // map removed fields to undefined
+  _.forOwn(base, (value, key) => {
+    if (!_.has(object, key)) result[key] = undefined;
+  });
+  return result;
+};
+
+export { serializeFormData, deepDiffObj };
