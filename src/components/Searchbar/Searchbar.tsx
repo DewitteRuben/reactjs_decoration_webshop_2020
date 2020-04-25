85149e01ab5709a86db10ca0bfc65b1cad0e064f
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-04-25 11:39:53 +0200

    feat: go to home page on searchbar enter

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -2,15 +2,33 @@ import React from "react";
 import TextInput from "../Input/Input";
 import { useStores } from "../../hooks/use-stores";
 import _ from "lodash";
+import { useHistory } from "react-router-dom";
 
 const Searchbar: React.FC = ({ ...props }) => {
   const { itemStore } = useStores();
+  const history = useHistory();
 
   const handleOnChange = (text: string) => {
     _.debounce(() => itemStore.fetchItemsBySearchString(text))();
   };
 
-  return <TextInput type="text" label="Search" onChangeText={handleOnChange} icon="search" {...props} />;
+  const handleOnEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const keyCode = event.keyCode || event.which;
+    if (keyCode === 13) {
+      return history.push("/");
+    }
+  };
+
+  return (
+    <TextInput
+      onKeyPress={handleOnEnter}
+      type="text"
+      label="Search"
+      onChangeText={handleOnChange}
+      icon="search"
+      {...props}
+    />
+  );
 };
 
 export default Searchbar;
